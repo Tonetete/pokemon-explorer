0c7acaa464e241e612adb390c2dd7e7a4b44d0c2
Antonio Rivas <toneterivas@gmail.com>
2025-03-13 22:07:52 +0100

    Added types

---
+++ src/types/index.ts
@@ -0,0 +1,55 @@
+import { FetchNextPageOptions, InfiniteQueryObserverResult } from '@tanstack/react-query'
+
+export interface QueryPokemonData {
+  data?: {
+    // Make data optional
+    pages: {
+      pokemonsDetails: PokemonDetail[]
+      nextOffset: number | null
+    }[] // Note: pages should be an array
+  }
+  isLoading: boolean
+  error: Error | null
+  fetchNextPage: (
+    options?: FetchNextPageOptions
+  ) => Promise<InfiniteQueryObserverResult<any, Error>>
+  hasNextPage: boolean
+}
+
+export interface PokemonDetail {
+  num: number
+  name: string
+  sprites: {
+    front_default: string
+  }
+  moves: {
+    move: {
+      name: string
+      url: string
+    }
+  }[]
+  types: {
+    slot: number
+    type: {
+      name: string
+      url: string
+    }
+  }[]
+  stats: {
+    base_stat: number
+    effort: number
+    stat: {
+      name: string
+      url: string
+    }
+  }[]
+}
+export interface PokemonListResponse {
+  count: number
+  next: string | null
+  previous: string | null
+  results: {
+    name: string
+    url: string
+  }[]
+}
