9568dbfd10ec287a28854d4ceecd0306bba042bb
Antonio Rivas <toneterivas@gmail.com>
2025-03-13 22:05:31 +0100

    Support tailwind css and resolve alias and reverse proxy to avoid CORS when querying pokemon api in vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import path from 'path'
+import { POKEMON_API_URL } from './src/constants'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: POKEMON_API_URL,
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
 })
