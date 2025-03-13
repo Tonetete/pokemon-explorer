06c8b4e3b4be40b6e3ff36cd273a09fda5af1889
Antonio Rivas <toneterivas@gmail.com>
2025-03-13 22:07:37 +0100

    Added Card component

---
+++ src/components/atoms/Card/Card.tsx
@@ -0,0 +1,7 @@
+export default function Card({ children }) {
+  return (
+    <div className="flex flex-col justify-center items-center h-48 w-48 max-w-md mx-auto bg-white rounded-xl shadow-md p-6">
+      {children}
+    </div>
+  )
+}
