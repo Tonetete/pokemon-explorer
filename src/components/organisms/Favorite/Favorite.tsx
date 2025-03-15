16ea2bfc5756b1eaac4a8a172e580fa1cf67c5cc
Antonio Rivas <toneterivas@gmail.com>
2025-03-15 21:34:43 +0100

    Data test id for Favorite component

--- src/components/organisms/Favorite/Favorite.tsx
+++ src/components/organisms/Favorite/Favorite.tsx
@@ -13,12 +13,22 @@ export default function FavoriteStar({ data, isFavorite, onToggle, children }: F
     <div>
       {children({
         favoriteButton: (
-          <button className="cursor-pointer" onClick={() => onToggle({ data, isFavorite })}>
+          <button
+            data-testid="favorite-button"
+            className="cursor-pointer"
+            onClick={() => onToggle({ data, isFavorite })}
+          >
             {isFavorite ? (
-              <FaStar className="text-yellow-500 text-xl transition-all transform scale-100" />
+              <FaStar
+                data-testid="star-icon"
+                className="text-yellow-500 text-xl transition-all transform scale-100"
+              />
             ) : (
               <div className="flex items-center gap-1 opacity-75 transition-all">
-                <FaRegStar className="text-yellow-500 text-xl opacity-50" />
+                <FaRegStar
+                  data-testid="unstar-icon"
+                  className="text-yellow-500 text-xl opacity-50"
+                />
               </div>
             )}
           </button>
